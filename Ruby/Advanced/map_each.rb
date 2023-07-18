59df7aecc9f8089f5113b2e6041bcf5a2aa954fc
hasan <hasanfarhan@hasans-MacBook-Air-2.local>
2023-07-18 18:44:46 +0530

    chore: added sql and other

---
+++ Ruby/Advanced/map_each.rb
@@ -0,0 +1,2 @@
+# What would be output for below 
+# [1,2,3,4].map{|a| a*2} [1,2,3,4].each{|a| a*2}
---
+++ Ruby/Advanced/truthy_and_falsy.rb
@@ -0,0 +1,43 @@
+# truthy and falsy concept
+if true
+puts 'TRUE'
+else 
+puts 'FALSE'
+end
+
+O/P: true
+
+    if []
+    puts 'TRUE'
+    else 
+    puts 'FALSE'
+    end
+
+
+    O/P: true
+
+    
+        if 0
+        puts 'TRUE'
+        else 
+        puts 'FALSE'
+        end
+
+    O/P: true
+
+        if nil
+        puts 'TRUE'
+        else 
+        puts 'FALSE'
+        end
+
+        O/P: false
+
+            if false
+            puts 'TRUE'
+            else 
+            puts 'FALSE'
+            end
+
+            O/P: false
+
